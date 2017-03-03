29da4153060344ccb8f28229796460d0d6c6ad3b
gabricampanella <gac2010@med.cornell.edu>
2017-03-03 14:16:17 -0500

    Update opts.lua

--- opts.lua
+++ opts.lua
@@ -31,6 +31,7 @@ function M.parse(arg)
    cmd:option('-nEpochs',         0,       'Number of total epochs to run')
    cmd:option('-batchSize',       32,      'mini-batch size (1 = pure stochastic)')
    cmd:option('-nClasses',         0,      'Number of classes in the dataset')
+   cmd:option('-finetune',       '',         'Path to model to finetune.')
    ---------- Optimization options ----------------------
    cmd:text('---------- Optimization options ----------------------')
    cmd:option('-LR',              0.1,   'initial learning rate')
